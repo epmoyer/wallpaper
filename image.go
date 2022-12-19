bf56f1d8e34c7867c79734ab165324b4b9aa6133
Eric Moyer <eric@lemoncrab.com>
2022-12-18 20:42:43 -0800

    wip, im2

--- image.go
+++ image.go
@@ -14,8 +14,99 @@ func main() {
 	var img *image.NRGBA
 	// img = createTestImage1(rect)
 	// save("image1.png", img)
-	img = createTestImage2(rect)
-	save("image2.png", img)
+	// img = createTestImage2(rect)
+	// save("image2.png", img)
+	// img = createTestImage3(rect)
+	// save("image3.png", img)
+	img = createTestImage4(rect)
+	save("image4.png", img)
+}
+
+func createTestImage4(rect image.Rectangle) (created *image.NRGBA) {
+	pix := make([]uint8, rect.Dx()*rect.Dy()*4)
+	stride := rect.Dx() * 4
+	// m1X := rect.Dx()
+	m1X := 0
+	m1Y := rect.Dy()
+	diagonal := math.Sqrt(float64(rect.Dx()*rect.Dx() + rect.Dy()*rect.Dy()))
+	for x := 0; x < rect.Dx(); x++ {
+		for y := 0; y < rect.Dy(); y++ {
+			m2Distance := distance(x, y, m1X, m1Y)
+			m2DistanceNorm := m2Distance / diagonal
+			yNorm := float64(y) / float64(rect.Dy())
+			angle := (float64)(x+y) / 5.4567
+			// angle := (float64)(x+y) / 10.4567
+			// angle2 := (float64)(x+y) / 20.199782
+			amplitude := math.Sin(angle)
+			// amplitude2 := math.Sin(angle2)
+			base := x*4 + y*stride
+			shade := uint8((amplitude+1)*5) + 150
+			pix[base] = shade - 100 + uint8(120*yNorm)
+			// pix[base+1] = shade - 50 + uint8((amplitude2+1)*10)
+			var bump uint8
+			// bump = 0
+			// if m2DistanceNorm < 0.5 {
+			// 	// if m2Distance == -100 {
+			// 	bump = uint8(30 * (1 - m2DistanceNorm))
+			// }
+			bump = uint8(75 * (1 - m2DistanceNorm))
+			pix[base+0] = 255
+			pix[base+1] = shade - 40 + bump
+			pix[base+2] = shade + 20 + bump
+			pix[base+3] = 255
+			// fmt.Println(x)
+		}
+	}
+	created = &image.NRGBA{
+		Pix:    pix,
+		Stride: rect.Dx() * 4,
+		Rect:   rect,
+	}
+	return
+}
+
+func createTestImage3(rect image.Rectangle) (created *image.NRGBA) {
+	pix := make([]uint8, rect.Dx()*rect.Dy()*4)
+	stride := rect.Dx() * 4
+	// m1X := rect.Dx()
+	m1X := 0
+	m1Y := rect.Dy()
+	diagonal := math.Sqrt(float64(rect.Dx()*rect.Dx() + rect.Dy()*rect.Dy()))
+	for x := 0; x < rect.Dx(); x++ {
+		for y := 0; y < rect.Dy(); y++ {
+			m2Distance := distance(x, y, m1X, m1Y)
+			m2DistanceNorm := m2Distance / diagonal
+			yNorm := float64(y) / float64(rect.Dy())
+			angle := (float64)(x+y) / 5.4567
+			// angle := (float64)(x+y) / 10.4567
+			// angle2 := (float64)(x+y) / 20.199782
+			amplitude := math.Sin(angle)
+			// amplitude2 := math.Sin(angle2)
+			base := x*4 + y*stride
+			shade := uint8((amplitude+1)*5) + 150
+			pix[base] = shade - 100 + uint8(120*yNorm)
+			// pix[base+1] = shade - 50 + uint8((amplitude2+1)*10)
+			var bump uint8
+			// bump = 0
+			// if m2DistanceNorm < 0.5 {
+			// 	// if m2Distance == -100 {
+			// 	bump = uint8(30 * (1 - m2DistanceNorm))
+			// }
+			bump = uint8(75 * (1 - m2DistanceNorm))
+			pix[base+0] = shade - 40 + bump
+			// pix[base+2] = shade + 30 + uint8(20*m2DistanceNorm)
+			pix[base+1] = shade + 20 + bump
+			pix[base+2] = 255
+			pix[base+3] = 255
+			// fmt.Println(x)
+		}
+	}
+	created = &image.NRGBA{
+		Pix:    pix,
+		Stride: rect.Dx() * 4,
+		Rect:   rect,
+	}
+	return
 }
 
 func createTestImage2(rect image.Rectangle) (created *image.NRGBA) {
@@ -31,6 +122,7 @@ func createTestImage2(rect image.Rectangle) (created *image.NRGBA) {
 			m2DistanceNorm := m2Distance / diagonal
 			yNorm := float64(y) / float64(rect.Dy())
 			angle := (float64)(x+y) / 5.4567
+			// angle := (float64)(x+y) / 10.4567
 			// angle2 := (float64)(x+y) / 20.199782
 			amplitude := math.Sin(angle)
 			// amplitude2 := math.Sin(angle2)
@@ -45,7 +137,7 @@ func createTestImage2(rect image.Rectangle) (created *image.NRGBA) {
 			// 	bump = uint8(30 * (1 - m2DistanceNorm))
 			// }
 			bump = uint8(75 * (1 - m2DistanceNorm))
-			pix[base+1] = shade - 50 + bump
+			pix[base+1] = shade - 40 + bump
 			// pix[base+2] = shade + 30 + uint8(20*m2DistanceNorm)
 			pix[base+2] = shade + 20 + bump
 			pix[base+3] = 255
