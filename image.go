9792d9836d6d858e1a4eb0c0b6da252f6cc4490d
Eric Moyer <eric@lemoncrab.com>
2022-12-19 11:00:31 -0800

    cull dead code

--- image.go
+++ image.go
@@ -165,7 +165,6 @@ func createTestImage2(rect image.Rectangle) (created *image.NRGBA) {
 	pix := make([]uint8, rect.Dx()*rect.Dy()*4)
 	stride := rect.Dx() * 4
 	m1X := rect.Dx()
-	// m1X := 0
 	m1Y := 0
 	diagonal := math.Sqrt(float64(rect.Dx()*rect.Dx() + rect.Dy()*rect.Dy()))
 	for x := 0; x < rect.Dx(); x++ {
@@ -174,29 +173,16 @@ func createTestImage2(rect image.Rectangle) (created *image.NRGBA) {
 			m2DistanceNorm := m2Distance / diagonal
 			yNorm := float64(y) / float64(rect.Dy())
 			angle := (float64)(x+y) / 5.4567
-			// angle := (float64)(x+y) / 10.4567
-			// angle2 := (float64)(x+y) / 20.199782
 			amplitude := math.Sin(angle)
-			// amplitude2 := math.Sin(angle2)
 			base := x*4 + y*stride
 			shade := uint8((amplitude+1)*5) + 150
 			pix[base] = shade - 100 + uint8(120*yNorm)
-			// pix[base+1] = shade - 50 + uint8((amplitude2+1)*10)
-			var bump uint8
-			// bump = 0
-			// if m2DistanceNorm < 0.5 {
-			// 	// if m2Distance == -100 {
-			// 	bump = uint8(30 * (1 - m2DistanceNorm))
-			// }
-			bump = uint8(75 * (1 - m2DistanceNorm))
+			bump := uint8(75 * (1 - m2DistanceNorm))
 			pix[base+1] = shade - 40 + bump
-			// pix[base+2] = shade + 30 + uint8(20*m2DistanceNorm)
 			pix[base+2] = shade + 20 + bump
 			pix[base+3] = 255
-			// fmt.Println(x)
 		}
 	}
-	// rand.Read(pix)
 	created = &image.NRGBA{
 		Pix:    pix,
 		Stride: rect.Dx() * 4,
@@ -205,7 +191,6 @@ func createTestImage2(rect image.Rectangle) (created *image.NRGBA) {
 	brighten(created, 0.23)
 	lighten(created, 0.01)
 	saturate(created, 0.10)
-	// colorTransform(created, 0.5, 0.5)
 	return
 }
 
