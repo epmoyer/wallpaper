cf96afb13b76b7b00f3aa6b0d423f59d93a1d1db
Eric Moyer <eric@lemoncrab.com>
2022-12-18 19:09:42 -0800

    wip, stripes

--- image.go
+++ image.go
@@ -5,13 +5,39 @@ import (
 	"image"
 	"image/png"
 	"log"
+	"math"
 	"os"
 )
 
 func main() {
 	rect := image.Rect(0, 0, 100, 100)
-	img := createRandomImage(rect)
+	img := createTestImage(rect)
 	save("random.png", img)
+}
+
+func createTestImage(rect image.Rectangle) (created *image.NRGBA) {
+	pix := make([]uint8, rect.Dx()*rect.Dy()*4)
+	stride := rect.Dx() * 4
+	for x := 0; x < rect.Dx(); x++ {
+		for y := 0; y < rect.Dy(); y++ {
+			angle := (float64)(x+y) / 1.4567
+			amplitude := math.Sin(angle)
+			base := x*4 + y*stride
+			shade := uint8((amplitude+1)*10) + 200
+			pix[base] = shade
+			pix[base+1] = shade
+			pix[base+2] = shade + 30
+			pix[base+3] = 255
+			// fmt.Println(x)
+		}
+	}
+	// rand.Read(pix)
+	created = &image.NRGBA{
+		Pix:    pix,
+		Stride: rect.Dx() * 4,
+		Rect:   rect,
+	}
+	return
 }
 
 func createRandomImage(rect image.Rectangle) (created *image.NRGBA) {
