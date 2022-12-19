d99730d660153c5749a82010872ff0f4790fabb8
Eric Moyer <eric@lemoncrab.com>
2022-12-19 12:26:48 -0800

    wip, purplegreen

--- image.go
+++ image.go
@@ -24,23 +24,110 @@ func main() {
 			Name: "m2_Air",
 			Rect: image.Rect(0, 0, 2560, 1664),
 		},
-		{
-			Name: "MacBookPro_16",
-			Rect: image.Rect(0, 0, 3072, 1920),
-		},
-		{
-			Name: "Dell_U4919DW",
-			Rect: image.Rect(0, 0, 5120, 1440),
-		},
+		// {
+		// 	Name: "MacBookPro_16",
+		// 	Rect: image.Rect(0, 0, 3072, 1920),
+		// },
+		// {
+		// 	Name: "Dell_U4919DW",
+		// 	Rect: image.Rect(0, 0, 5120, 1440),
+		// },
 	}
 	for _, display := range displays {
 		var img *image.NRGBA
 		rect := display.Rect
-		filename := fmt.Sprintf("wallpaper_plumset_%s_%dx%d.png", display.Name, rect.Dx(), rect.Dy())
+
+		// filename := fmt.Sprintf("wallpaper_plumset_%s_%dx%d.png", display.Name, rect.Dx(), rect.Dy())
+		// fmt.Printf("Creating %s...\n", filename)
+		// img = createPlumset(rect)
+		// save(filename, img)
+
+		filename := fmt.Sprintf("wallpaper_WIP_%s_%dx%d.png", display.Name, rect.Dx(), rect.Dy())
 		fmt.Printf("Creating %s...\n", filename)
-		img = createPlumset(rect)
+		img = createWIP(rect)
 		save(filename, img)
 	}
+}
+
+func createWIP(rect image.Rectangle) *image.NRGBA {
+	var r, g, b float64
+	width := rect.Dx()
+	height := rect.Dy()
+	pix := make([]uint8, width*height*4)
+	stride := width * 4
+	m1X := width / 2
+	m1Y := 0
+	diagonal := math.Sqrt(float64(width*width + height*height))
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			m2Distance := distance(x, y, m1X, m1Y)
+			m2DistanceNorm := m2Distance / diagonal
+			bump := 72 * (1 - m2DistanceNorm)
+			yNorm := float64(y) / float64(height)
+			angle := (float64)(x+width-y) / 5.4567
+			amplitude := math.Sin(angle)
+			base := x*4 + y*stride
+			shade := (amplitude + 1) * 5
+			fade := 70 * yNorm
+
+			r = 128 + 10 + shade
+			g = 128 + shade*1.2 + fade
+			b = 128 + 30 + shade + bump
+
+			pix[base+0] = uint8(r)
+			pix[base+1] = uint8(g)
+			pix[base+2] = uint8(b)
+			pix[base+3] = 255
+		}
+	}
+	img := &image.NRGBA{
+		Pix:    pix,
+		Stride: rect.Dx() * 4,
+		Rect:   rect,
+	}
+	// brighten(img, 0.23)
+	// lighten(created, 0.03)
+	// saturate(img, 0.25)
+	// hue(img, -55)
+	// created =
+	// img = imaging.Blur(img, 3.2)
+	// img = imaging.(img, 3.2)
+	return img
+}
+
+func createPlumset(rect image.Rectangle) *image.NRGBA {
+	pix := make([]uint8, rect.Dx()*rect.Dy()*4)
+	stride := rect.Dx() * 4
+	m1X := rect.Dx()
+	m1Y := 0
+	diagonal := math.Sqrt(float64(rect.Dx()*rect.Dx() + rect.Dy()*rect.Dy()))
+	for x := 0; x < rect.Dx(); x++ {
+		for y := 0; y < rect.Dy(); y++ {
+			m2Distance := distance(x, y, m1X, m1Y)
+			m2DistanceNorm := m2Distance / diagonal
+			yNorm := float64(y) / float64(rect.Dy())
+			angle := (float64)(x+y) / 5.4567
+			amplitude := math.Sin(angle)
+			base := x*4 + y*stride
+			shade := uint8((amplitude+1)*5) + 150
+			pix[base] = shade - 100 + uint8(120*yNorm)
+			bump := uint8(72 * (1 - m2DistanceNorm))
+			pix[base+1] = shade - 40 + bump
+			pix[base+2] = shade + 20 + bump
+			pix[base+3] = 255
+		}
+	}
+	img := &image.NRGBA{
+		Pix:    pix,
+		Stride: rect.Dx() * 4,
+		Rect:   rect,
+	}
+	brighten(img, 0.23)
+	// lighten(created, 0.03)
+	saturate(img, 0.25)
+	// created =
+	img = imaging.Blur(img, 3.2)
+	return img
 }
 
 func createTestImage4(rect image.Rectangle) (created *image.NRGBA) {
@@ -136,6 +223,23 @@ func saturate(image *image.NRGBA, saturatePercent float64) {
 	}
 }
 
+func hue(image *image.NRGBA, degrees float64) {
+	var r, g, b float64
+	for x := 0; x < image.Rect.Dx(); x++ {
+		for y := 0; y < image.Rect.Dy(); y++ {
+			base := x*4 + y*image.Stride
+			c := noire.NewRGB(
+				float64(image.Pix[base+0]),
+				float64(image.Pix[base+1]),
+				float64(image.Pix[base+2]))
+			r, g, b = c.AdjustHue(degrees).RGB()
+			image.Pix[base+0] = uint8(r)
+			image.Pix[base+1] = uint8(g)
+			image.Pix[base+2] = uint8(b)
+		}
+	}
+}
+
 func createTestImage3(rect image.Rectangle) (created *image.NRGBA) {
 	pix := make([]uint8, rect.Dx()*rect.Dy()*4)
 	stride := rect.Dx() * 4
@@ -178,41 +282,6 @@ func createTestImage3(rect image.Rectangle) (created *image.NRGBA) {
 		Rect:   rect,
 	}
 	return
-}
-
-func createPlumset(rect image.Rectangle) *image.NRGBA {
-	pix := make([]uint8, rect.Dx()*rect.Dy()*4)
-	stride := rect.Dx() * 4
-	m1X := rect.Dx()
-	m1Y := 0
-	diagonal := math.Sqrt(float64(rect.Dx()*rect.Dx() + rect.Dy()*rect.Dy()))
-	for x := 0; x < rect.Dx(); x++ {
-		for y := 0; y < rect.Dy(); y++ {
-			m2Distance := distance(x, y, m1X, m1Y)
-			m2DistanceNorm := m2Distance / diagonal
-			yNorm := float64(y) / float64(rect.Dy())
-			angle := (float64)(x+y) / 5.4567
-			amplitude := math.Sin(angle)
-			base := x*4 + y*stride
-			shade := uint8((amplitude+1)*5) + 150
-			pix[base] = shade - 100 + uint8(120*yNorm)
-			bump := uint8(72 * (1 - m2DistanceNorm))
-			pix[base+1] = shade - 40 + bump
-			pix[base+2] = shade + 20 + bump
-			pix[base+3] = 255
-		}
-	}
-	img := &image.NRGBA{
-		Pix:    pix,
-		Stride: rect.Dx() * 4,
-		Rect:   rect,
-	}
-	brighten(img, 0.23)
-	// lighten(created, 0.03)
-	saturate(img, 0.25)
-	// created =
-	img = imaging.Blur(img, 3.2)
-	return img
 }
 
 func distance(x1 int, y1 int, x2 int, y2 int) float64 {
