f84dfe32526ce16a5eb46ce1c86b5e7b0ec90d28
Eric Moyer <eric@lemoncrab.com>
2022-12-20 07:40:53 -0800

    fc:triangle wave

--- image.go
+++ image.go
@@ -53,20 +53,25 @@ func main() {
 		// img = createMelon(rect, display)
 		// save(filename, img)
 
-		filename = fmt.Sprintf("wallpaper_bluehatch_%s_%dx%d.png", display.Name, rect.Dx(), rect.Dy())
+		filename = fmt.Sprintf("wallpaper_bluehatch_tri_%s_%dx%d.png", display.Name, rect.Dx(), rect.Dy())
 		fmt.Printf("Creating %s...\n", filename)
-		img = createBlueHatch(rect, display)
+		img = createBlueHatchTri(rect, display)
 		save(filename, img)
 
-		filename = fmt.Sprintf("wallpaper_purplehatch_%s_%dx%d.png", display.Name, rect.Dx(), rect.Dy())
-		fmt.Printf("Creating %s...\n", filename)
-		img = createPurpleHatch(rect, display)
-		save(filename, img)
+		// filename = fmt.Sprintf("wallpaper_bluehatch_%s_%dx%d.png", display.Name, rect.Dx(), rect.Dy())
+		// fmt.Printf("Creating %s...\n", filename)
+		// img = createBlueHatch(rect, display)
+		// save(filename, img)
 
-		filename = fmt.Sprintf("wallpaper_greenhatch_%s_%dx%d.png", display.Name, rect.Dx(), rect.Dy())
-		fmt.Printf("Creating %s...\n", filename)
-		img = createGreenHatch(rect, display)
-		save(filename, img)
+		// filename = fmt.Sprintf("wallpaper_purplehatch_%s_%dx%d.png", display.Name, rect.Dx(), rect.Dy())
+		// fmt.Printf("Creating %s...\n", filename)
+		// img = createPurpleHatch(rect, display)
+		// save(filename, img)
+
+		// filename = fmt.Sprintf("wallpaper_greenhatch_%s_%dx%d.png", display.Name, rect.Dx(), rect.Dy())
+		// fmt.Printf("Creating %s...\n", filename)
+		// img = createGreenHatch(rect, display)
+		// save(filename, img)
 
 		// filename = fmt.Sprintf("wallpaper_bluedrops_%s_%dx%d.png", display.Name, rect.Dx(), rect.Dy())
 		// fmt.Printf("Creating %s...\n", filename)
@@ -112,6 +117,56 @@ func createBlueHatch(rect image.Rectangle, display displayT) *image.NRGBA {
 			amplitude := math.Sin(angle)
 			base := x*4 + y*stride
 			shade := (amplitude + 1) * 4
+			fade := 80 * (1 - yNorm)
+
+			r = 50 + 10 + shade + fade
+			g = 50 + 40 + shade*1.2 + fade
+			b = 50 + 70 + shade + fade + 0.00001*bump
+
+			pix[base+0] = uint8(r)
+			pix[base+1] = uint8(g)
+			pix[base+2] = uint8(b)
+			pix[base+3] = 255
+		}
+	}
+	img := &image.NRGBA{
+		Pix:    pix,
+		Stride: rect.Dx() * 4,
+		Rect:   rect,
+	}
+	// brighten(img, 0.1)
+	// lighten(created, 0.03)
+	saturate(img, 0.1)
+	// hue(img, -55)
+	// created =
+	// img = imaging.Blur(img, 3.2)
+	img = imaging.AdjustBrightness(img, -20)
+	return img
+}
+
+func createBlueHatchTri(rect image.Rectangle, display displayT) *image.NRGBA {
+	var r, g, b float64
+	width := rect.Dx()
+	height := rect.Dy()
+	pix := make([]uint8, width*height*4)
+	stride := width * 4
+	// m1X := width / 2
+	m1X := 0
+	m1Y := 0
+	diagonal := math.Sqrt(float64(width*width + height*height))
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			m2Distance := distance(x, y, m1X, m1Y)
+			m2DistanceNorm := m2Distance / diagonal
+			bump := 72 * (1 - m2DistanceNorm)
+			yNorm := float64(y) / float64(height)
+			angle := (float64)(x+width-y) / 2.12345
+			angle *= float64(BASELINE_PPI) / float64(display.PPI)
+			// angle /= 10
+			// amplitude := math.Sin(angle)
+			amplitude := (math.Abs(math.Mod(angle, 2*math.Pi)-math.Pi) - (math.Pi / 2)) / (math.Pi / 2)
+			base := x*4 + y*stride
+			shade := (amplitude + 1) * 10
 			fade := 80 * (1 - yNorm)
 
 			r = 50 + 10 + shade + fade
